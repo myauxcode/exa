5158ee028256185d5484c5a74429ef3b7e5589c9
myauxcode <104797580+myauxcode@users.noreply.github.com>
2023-05-22 23:00:49 -0400

    Add files via upload

---
+++ PruebaS/PruebaS/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace PruebaS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.eXAMENToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eJERCICIOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(33, 240);
+            this.textBox10.Multiline = true;
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(738, 78);
+            this.textBox10.TabIndex = 19;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(33, 172);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(738, 20);
+            this.textBox9.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(459, 91);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(353, 91);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(247, 91);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "NUMERICO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "TEXTO";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eXAMENToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(890, 24);
+            this.menuStrip1.TabIndex = 22;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // eXAMENToolStripMenuItem
+            // 
+            this.eXAMENToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eJERCICIOToolStripMenuItem});
+            this.eXAMENToolStripMenuItem.Name = "eXAMENToolStripMenuItem";
+            this.eXAMENToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.eXAMENToolStripMenuItem.Text = "EXAMEN";
+            // 
+            // eJERCICIOToolStripMenuItem
+            // 
+            this.eJERCICIOToolStripMenuItem.Name = "eJERCICIOToolStripMenuItem";
+            this.eJERCICIOToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.eJERCICIOToolStripMenuItem.Text = "EJERCICIO";
+            this.eJERCICIOToolStripMenuItem.Click += new System.EventHandler(this.eJERCICIOToolStripMenuItem_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(151, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "vi1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(257, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "r1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(360, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "vi2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(471, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 13);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "r2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 374);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem eXAMENToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eJERCICIOToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ PruebaS/PruebaS/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //EXAMEN SIMULACION
+
+
+        public double ejercicio7(int n, double vi1, double r1, double vi2, double r2)
+        {
+            double f, t1, t2;
+            f = 0;
+            bool ba;
+            ba = true;
+
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                t1 = vi1 + (i - 1) * r1;
+                t2 = vi2 + (i - 1) * r2;
+
+                if (ba == true)
+                {
+                    f = f - Math.Sqrt((Math.Sin(t1)) / (Fact(t2) / 2));
+                }
+                else
+                {
+                    f = f + Math.Sqrt((Math.Sin(t1)) / (Fact(t2) / 2));
+                }
+
+            }
+            return f;
+        }
+
+        public string ejercicio7t(int n, double vi1, double r1, double vi2, double r2)
+        {
+            double t1, t2;
+            string f, t;
+            f = "";
+            t = "";
+            bool ba;
+            ba = true;
+
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                t1 = vi1 + (i - 1) * r1;
+                t2 = vi2 + (i - 1) * r2;
+
+                if (ba == true)
+                {
+                    f = f + "   -     Sqrt((Sin(" + t1 + ")) / ((" + t2 + ")! / " + 2 + "))";
+                    ba = !ba;
+                }
+                else
+                { 
+                    f = f + "   +      Sqrt((Sin(" + t1 + ")) / ((" + t2 + ")! / " + 2 + "))";
+                    ba = !ba;
+                }
+
+            }
+            return f;
+        }
+        public double Fact(double n)
+        {
+            double f;
+            double i;
+
+
+            if (n != 0)
+            {
+                f = 1;
+                for (i = Math.Abs(n); i >= 1; i--)
+                {
+                    f = f * i;
+                    if (n < 0)
+                    {
+                        f = -f;
+                    }
+
+                }
+            }
+            else
+            {
+                f = 1;
+            }
+
+            return f;
+        }
+        private void eJERCICIOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox9.Text = string.Concat(ejercicio7(int.Parse(textBox1.Text), double.Parse(textBox2.Text), double.Parse(textBox3.Text), double.Parse(textBox4.Text), double.Parse(textBox5.Text)));
+
+            textBox10.Text = string.Concat(ejercicio7t(int.Parse(textBox1.Text), double.Parse(textBox2.Text), double.Parse(textBox3.Text), double.Parse(textBox4.Text), double.Parse(textBox5.Text)));
+        }
+    }
+}
